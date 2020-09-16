628f4ec4a88404ae7e7820e1867759fc497fd081
naufalzhafran <latifnaufal7@gmail.com>
2020-09-16 09:11:50 +0700

    add boilerplate

---
+++ layers.py
@@ -0,0 +1,25 @@
+class ConvLayer:
+  def __init__(self,filter_size,num_filter,input_size,channel_size,stride,padding):
+    pass
+
+  def foward(self,input):
+    pass
+
+class PoolLayer:
+  def __init__(self,filter_size,stride_size,mode):
+    pass
+  def foward(self,input):
+    # input array of feature map -> output array of feature map
+    pass
+
+class FlattenLayer:
+  def init(self):
+    pass
+  def foward(self,input):
+    pass
+
+class DenseLayer:
+  def __init__(self,n_units,activation):
+    pass
+  def foward(self,input):
+    pass
