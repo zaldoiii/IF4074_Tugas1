5ca3416694597ee903f339c05aa6a87c1dbd054d
naufalzhafran <latifnaufal7@gmail.com>
2020-09-16 19:18:03 +0700

    just test

--- layers.py
+++ layers.py
@@ -1,6 +1,17 @@
+import numpy as np
+
 class ConvLayer:
-  def __init__(self,filter_size,num_filter,input_size,channel_size,stride,padding):
-    pass
+  def __init__(self,filter_size,num_filter,input_size,num_channel,stride=1,padding=0):
+    self._filter_size = filter_size
+    self._num_filter = num_filter
+    self._input_size = input_size
+    self._num_channel = num_channel
+    self._stride = stride
+    self._padding = padding
+
+    self._weight =  np.random.randn(num_filter,num_channel,filter_size,filter_size) 
+    self._bias = np.zeros((num_filter,num_channel))
+    print(self._weight)
 
   def foward(self,input):
     pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from layers import ConvLayer
+
+if __name__ == "__main__":
+    layer1 = ConvLayer(3,1,32,1,1,0)
