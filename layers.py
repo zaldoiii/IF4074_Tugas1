c14621daa31cad218c7870087690ff82efcfa8bf
naufalzhafran <latifnaufal7@gmail.com>
2020-09-17 11:49:55 +0700

    implement padding and foward propagation

--- layers.py
+++ layers.py
@@ -9,12 +9,37 @@ class ConvLayer:
     self._stride = stride
     self._padding = padding
 
-    self._weight =  np.random.randn(num_filter,num_channel,filter_size,filter_size) 
+    self._weights =  np.random.randn(num_filter,num_channel,filter_size,filter_size) 
     self._bias = np.zeros((num_filter,num_channel))
-    print(self._weight)
+    print(self._weights)
 
-  def foward(self,input):
-    pass
+  def zero_padding(self, inputs):
+    w, h = inputs.shape[0], inputs.shape[1]
+    new_w = 2 * self._padding + w
+    new_h = 2 * self._padding + h
+    output = np.zeros((new_w, new_h))
+    output[self._padding:w+self._padding, self._padding:h+self._padding] = inputs
+    return output
+
+  def foward(self,inputs):
+    channel_size = inputs.shape[0]
+    width = inputs.shape[1]+2*self._padding
+    height = inputs.shape[2]+2*self._padding
+
+    self._inputs = np.zeros((channel_size, width, height))
+    for c in range(inputs.shape[0]):
+        self.inputs[c,:,:] = self.zero_padding(inputs[c,:,:])
+
+    out_width = (width - self._filter_size)/self._stride + 1
+    out_heigth = (heigth - self._filter_size)/self._stride + 1
+    feature_maps = np.zeros((self.F, out_width, out_heigth))
+
+    for f in range(self._num_filter):
+        for w in range(out_width):
+            for h in range(out_heigth):
+                feature_maps[f,w,h]=np.sum(self._inputs[:, w:w+self._filter_size, h:h+self._filter_size] * self._weights[f,:,:,:]) + self._bias[f]
+
+    return feature_maps
 
 class PoolLayer:
   def __init__(self,filter_size,stride_size,mode):
